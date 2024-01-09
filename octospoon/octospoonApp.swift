2835230dcbf44727ffeef646160ef6bcb00688c3
Ben Frearson <120649330+benfrearson@users.noreply.github.com>
2024-01-09 11:42:42 +0000

    Initial Commit

---
+++ octospoon/octospoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  octospoonApp.swift
+//  octospoon
+//
+//  Created by Ben Frearson on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct octospoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
